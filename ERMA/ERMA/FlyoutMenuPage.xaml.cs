9a20ad257f0357e78fd2afb7b3eef164ee512797
Sylvester Eze <sylvesterchima11@outlook.com>
2022-04-01 23:31:09 +0100

    implemented flyout menu

--- ERMA/ERMA/FlyoutMenuPage.xaml.cs
+++ ERMA/ERMA/FlyoutMenuPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ERMA.Helper;
+using ERMA.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,57 @@ namespace ERMA
         public FlyoutMenuPage()
         {
             InitializeComponent();
+            //get the user zone
+            lblZone.Text = "Magodo South-East Zone";
+        }
+
+        private void Dashboard_Tapped(object sender, EventArgs e)
+        {
+            MainPage mRootDetailPage = (MainPage)(App.Current.MainPage);
+            mRootDetailPage.Detail = PageUtility.CreateDashboardPage();
+            mRootDetailPage.IsPresented = false;
+        }
+
+        private void Dues_Tapped(object sender, EventArgs e)
+        {
+            MainPage mRootDetailPage = (MainPage)(App.Current.MainPage);
+            mRootDetailPage.Detail = PageUtility.CreateDuesPage();
+            mRootDetailPage.IsPresented = false;
+        }
+
+        private void Messages_Tapped(object sender, EventArgs e)
+        {
+            MainPage mRootDetailPage = (MainPage)(App.Current.MainPage);
+            mRootDetailPage.IsPresented = false;
+        }
+
+        private void Vendors_Tapped(object sender, EventArgs e)
+        {
+            MainPage mRootDetailPage = (MainPage)(App.Current.MainPage);
+            mRootDetailPage.Detail = PageUtility.CreateVendorsPage();
+            mRootDetailPage.IsPresented = false;
+        }
+
+
+        private void Complaints_Tapped(object sender, EventArgs e)
+        {
+            MainPage mRootDetailPage = (MainPage)(App.Current.MainPage);
+            mRootDetailPage.IsPresented = false;
+        }
+
+        private void BackArrow_Tapped(object sender, EventArgs e)
+        {
+            MessagingCenter.Send("Open Menu", MessageCenterTypes.M_C_SHOW_SIDE_MENU);
+        }
+
+        private void BG_Tapped(object sender, EventArgs e)
+        {
+            MessagingCenter.Send("Open Menu", MessageCenterTypes.M_C_SHOW_SIDE_MENU);
+        }
+
+        private void Content_Tapped(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- ERMA/ERMA/Utilities/PageUtility.cs
+++ ERMA/ERMA/Utilities/PageUtility.cs
@@ -24,5 +24,39 @@ namespace ERMA.Utilities
 
             return nav;
         }
+
+        public static Page CreateDashboardPage()
+        {
+            Page page = FreshMvvm.FreshPageModelResolver.ResolvePageModel<DashboardViewModel>();
+            NavigationPage nav = new FreshMvvm.FreshNavigationContainer(page) as NavigationPage;
+
+            nav.BarBackgroundColor = Color.FromHex(PageConstants.PrimaryColor);
+            nav.BarTextColor = Color.FromHex(PageConstants.White);
+
+            return nav;
+        }
+
+        public static Page CreateVendorsPage()
+        {
+            Page page = FreshMvvm.FreshPageModelResolver.ResolvePageModel<VendorsViewModel>();
+            NavigationPage nav = new FreshMvvm.FreshNavigationContainer(page) as NavigationPage;
+
+            nav.BarBackgroundColor = Color.FromHex(PageConstants.PrimaryColor);
+            nav.BarTextColor = Color.FromHex(PageConstants.White);
+
+            return nav;
+        }
+
+        public static Page CreateDuesPage()
+        {
+            Page page = FreshMvvm.FreshPageModelResolver.ResolvePageModel<DuesMakePaymentViewModel>();
+            NavigationPage nav = new FreshMvvm.FreshNavigationContainer(page) as NavigationPage;
+
+            nav.BarBackgroundColor = Color.FromHex(PageConstants.PrimaryColor);
+            nav.BarTextColor = Color.FromHex(PageConstants.White);
+
+            return nav;
+        }
+
     }
 }
